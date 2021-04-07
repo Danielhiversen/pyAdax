2984ffc69763e3a27de4d4f22584dac2ec60edda
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-04-07 16:44:30 +0200

    init
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pyAdax
-Python library for Adax heater
+
+Python3 library for Adax heater. 
+
+Control Adax heaters and get measured temperatures.
+
+[Buy me a coffee :)](http://paypal.me/dahoiv)
+
+
+
+## Install
+```
+pip3 install adax
+```
+
+## Example:
+
+```python
+import adax
+
+
+```
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='adax',
+    packages=['adax'],
+    install_requires=['aiohttp>=3.0.6', 'async_timeout>=1.4.0'],
+    version='0.0.1',
+    description='A python3 library to communicate with Adax',
+    long_description='A python3 library to communicate with Adax',
+    python_requires='>=3.5.3',
+    author='Daniel Hjelseth Hoyer',
+    author_email='mail@dahoiv.net',
+    url='https://github.com/Danielhiversen/pyAdax',
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
